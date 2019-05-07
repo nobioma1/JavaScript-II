c1901effdecadbf092b8ffc603fe378b310a711a
Noble Obioma <nobioma1@gmail.com>
2019-05-07 13:48:45 +0100

    Challenge on closure - write simple example of a closure and invoke function - Counter Function:     -- declare count variable and set it to zero     -- return a function that increments the count variable.     -- assign return value of counter function to new counter     -- invoke new counter 4 times to show increment - Counter Factory:     -- declare variable count and set it to 0     -- counterFactory return object containing methods increment and decrement     -- increment is a method that adds one to count and returns count     -- decrement is a method that removes one from count and returns count     -- set counterFactory return value to counterFactoryItems     -- invoke increment 3times and decrememnt 2times to test functions. - update README.md

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,62 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function country() {
+  const president = 'Mr. President';
+  function state() {
+    const governor = 'Mr. Governor';
+    console.log(`His Excellency ${governor} welcomes ${president}`);
+    function lga() {
+      const chairman = 'Mr. Chiarman';
+      console.log(
+        `The Local Goverment Area ${chairman} welcomes ${president} and ${governor}`
+      );
+    }
+    lga();
+  }
+  state();
+}
 
+country();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
   // Return a function that when invoked increments and returns a counter variable.
+  return function() {
+    ++count;
+    return count;
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage:
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
+console.log(newCounter()); // 3
+console.log(newCounter()); // 4
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0;
   // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  return {
+    // `increment` should increment a counter variable in closure scope and return it.
+    increment: function() {
+      ++count;
+      return count;
+    },
+    // `decrement` should decrement the counter variable and return it.
+    decrement: function() {
+      --count;
+      return count;
+    }
+  };
 };
+
+const counterFactoryItems = counterFactory();
+console.log(counterFactoryItems.increment()); // 1
+console.log(counterFactoryItems.increment()); // 2
+console.log(counterFactoryItems.increment()); // 3
+console.log(counterFactoryItems.decrement()); // 2
+console.log(counterFactoryItems.decrement()); // 1
